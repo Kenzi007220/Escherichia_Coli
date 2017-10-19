14c3dc1dbe72255f566605d6b5d4cd3e0032b7fe
Oleksandr <kenzi@ukr.net>
2017-10-19 14:06:02 +0300

    Implementations added

---
+++ src/main/java/bacteria/Coordinates.java
@@ -0,0 +1,13 @@
+package bacteria;
+
+public class Coordinates {
+
+  private final int x;
+  private final int y;
+
+
+  public Coordinates(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
